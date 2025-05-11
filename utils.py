20030250a6f8a247714f7fdcb081c3f1ac746732
yuki-oka <55942183+fate7bardiche@users.noreply.github.com>
2025-05-11 17:12:32 +0900

    feat: ファイル名を取得する関数を実装

--- utils.py
+++ utils.py
@@ -10,8 +10,14 @@ def calc_readble_file_bytes(media_type: str, json_data: any):
     json_str_bits_len = len(json.dumps(json_data).encode())
     return config.payload_byte_size - media_type_bits_len - json_str_bits_len
 
+# hoge/fuga.mp4 -> fuga.mp4
+def get_file_name(file_path: str):
+    return file_path[file_path.rfind("/") + 1:]
+
+# hoge/fuga.mp4 -> fuga
 def get_file_stem(file_name: str):
     return file_name[ file_name.rfind("/") + 1 : file_name.rfind(".")]
 
+# hoge/fuga.mp4 -> mp4
 def get_file_extension(file_name: str):
     return file_name[file_name.rfind(".") + 1:]
